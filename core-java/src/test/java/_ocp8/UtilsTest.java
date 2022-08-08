b46a466a97dbed4cd536667a8fa2b8c3d0bd94f0
hsmak <husain.alkhamis@gmail.com>
2022-08-08 18:31:03 -0500

    committing changes...

--- core-java/src/test/java/_ocp8/UtilsTest.java
+++ core-java/src/test/java/_ocp8/UtilsTest.java
@@ -10,31 +10,45 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UtilsTest {
 
     @Test
-    public void getSlidingStream() {
-//        System.out.println(Utils.getSlidingStream("0123456789".toCharArray(), 3, 2).collect(Collectors.toList()));
-
-        List<String> list01 = Utils.getSlidingStream("0123456789".toCharArray(), 3, 2)
+    public void getSlidingStream_Size_01_Step_01() {
+        List<String> list = Utils.getSlidingStream("0123456789".toCharArray(), 1, 1)
                 .collect(Collectors.toList());
-        list01.stream().collect(Collectors.joining(",", "[", "]")).chars().mapToObj(Character::toString).forEach(System.out::print);
-        assertThat(list01)
-                .containsExactlyElementsOf(List.of("012", "234", "456", "678", "89"));
-        System.out.println();
-
-        List<String> list02 = Utils.getSlidingStream("0123456789".toCharArray(), 1, 1)
-                .collect(Collectors.toList());
-        list02.stream().collect(Collectors.joining(",", "[", "]")).chars().mapToObj(Character::toString).forEach(System.out::print);
-        assertThat(list02)
+        list.stream().collect(Collectors.joining(",", "[", "]")).chars().mapToObj(Character::toString).forEach(System.out::print);
+        assertThat(list)
                 .containsExactlyElementsOf(List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"));
         System.out.println();
+    }
 
-        //ToDo
-        assertThat(Utils.getSlidingStream("0123456789".toCharArray(), 2, 2)
-                .collect(Collectors.toList()))
-                .containsExactlyElementsOf(List.of("01", "23", "45", "67", "89"));
+    @Test
+    public void getSlidingStream_Size_03_Step_02() {
+        List<String> list = Utils.getSlidingStream("0123456789".toCharArray(), 3, 2)
+                .collect(Collectors.toList());
+        list.stream().collect(Collectors.joining(",", "[", "]")).chars().mapToObj(Character::toString)
+                .forEach(System.out::print);
 
-        //ToDo
-        assertThat(Utils.getSlidingStream("0123456789".toCharArray(), 4, 2)
-                .collect(Collectors.toList()))
+        assertThat(list)
+                .containsExactlyElementsOf(List.of("012", "234", "456", "678", "89"));
+    }
+
+    @Test
+    public void getSlidingStream_Size_04_Step_02() {
+        List<String> list = Utils.getSlidingStream("0123456789".toCharArray(), 4, 2)
+                .collect(Collectors.toList());
+        list.stream().collect(Collectors.joining(",", "[", "]")).chars().mapToObj(Character::toString)
+                .forEach(System.out::print);
+
+        assertThat(list)
                 .containsExactlyElementsOf(List.of("0123", "2345", "4567", "6789", "89"));
+    }
+
+    @Test
+    public void getSlidingStream_Size_02_Step_02() {
+        List<String> list = Utils.getSlidingStream("0123456789".toCharArray(), 2, 2)
+                .collect(Collectors.toList());
+        list.stream().collect(Collectors.joining(",", "[", "]")).chars().mapToObj(Character::toString)
+                .forEach(System.out::print);
+
+        assertThat(list)
+                .containsExactlyElementsOf(List.of("01", "23", "45", "67", "89"));
     }
 }
